274af88259bc586d637ca5572aa74e2cb3b5c6f3
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-07-04 14:35:08 +0200

    Code cleanup

--- Naloga4/Naloga4/src/naloga4/Naloga4.java
+++ Naloga4/Naloga4/src/naloga4/Naloga4.java
@@ -3,19 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package naloga3;
+package naloga4;
 
 /**
  *
  * @author seba
  */
-public class Naloga3 {
+public class Naloga4 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new Aplikacija().setVisible(true);
+        new MainWindow().setVisible(true);
     }
-    
+
 }
